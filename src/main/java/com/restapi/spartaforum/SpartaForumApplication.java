dde97a3d5cc356e5d8c1d6cd98c1eb95a4a96bdc
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-05 19:21:14 +0900

    init : spring boot init

---
+++ src/main/java/com/restapi/spartaforum/SpartaForumApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.spartaforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaForumApplication.class, args);
+	}
+
+}
