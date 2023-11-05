8dceda939da7e2aec36137f82f018a94b3eff60a
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-06 06:24:58 +0900

    feat : 기존 유저가 있다면 조회,없다면 생성 이후, 게시글 생성

--- src/main/java/com/restapi/spartaforum/service/QuestionService.java
+++ src/main/java/com/restapi/spartaforum/service/QuestionService.java
@@ -1,7 +1,56 @@
 package com.restapi.spartaforum.service;
 
+import com.restapi.spartaforum.domain.dto.QuestionMapper;
+import com.restapi.spartaforum.domain.dto.QuestionRequestDto;
+import com.restapi.spartaforum.domain.dto.QuestionResponseDto;
+import com.restapi.spartaforum.domain.entity.Question;
+import com.restapi.spartaforum.domain.entity.User;
+import com.restapi.spartaforum.domain.repo.QuestionRepo;
+import com.restapi.spartaforum.domain.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class QuestionService {
+    private final QuestionRepo questionRepo;
+    private final UserRepo userRepo;
+
+    public ResponseEntity<QuestionResponseDto> createPost(QuestionRequestDto requestDto) {
+        // author를 통해 고객 조회, 없다면 고객 생성
+        String author = requestDto.getAuthor();
+        String password = requestDto.getPassword();
+        User user = getOrCreateUserIfNotExists(author, password);
+
+        // 게시글 생성
+        Question question = Question.builder()
+                .title(requestDto.getTitle())
+                .author(requestDto.getAuthor())
+                .password(requestDto.getPassword())
+                .content(requestDto.getContent())
+                .user(user).build();
+
+        // 게시글 저장
+        Question savedQuestion = questionRepo.save(question);
+
+        // 반환 DTO 매핑
+        QuestionResponseDto questionResponseDto = QuestionMapper.INSTANCE.questionToResponseDto(savedQuestion);
+
+        // 반환
+        return new ResponseEntity<>(questionResponseDto, HttpStatus.OK);
+    }
+
+    final User getOrCreateUserIfNotExists(String name, String password) {
+        User user = userRepo.findUserByName(name);
+        if (user == null) {
+            user = userRepo.save(
+                    User.builder()
+                            .name(name)
+                            .password(password).build()
+            );
+        }
+        return user;
+    }
 }
