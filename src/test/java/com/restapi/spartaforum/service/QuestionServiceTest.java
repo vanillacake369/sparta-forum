0043fe396573af4fa53d15ec21ad617a069a2346
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-06 07:32:09 +0900

    test : Service > postId를 통해 게시글 정상/예외 케이스 검증

--- src/test/java/com/restapi/spartaforum/service/QuestionServiceTest.java
+++ src/test/java/com/restapi/spartaforum/service/QuestionServiceTest.java
@@ -1,7 +1,10 @@
 package com.restapi.spartaforum.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.restapi.spartaforum.domain.dto.QuestionResponseDto;
+import com.restapi.spartaforum.domain.entity.Question;
 import com.restapi.spartaforum.domain.entity.User;
 import com.restapi.spartaforum.domain.repo.QuestionRepo;
 import com.restapi.spartaforum.domain.repo.UserRepo;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 
 
 @SpringBootTest
@@ -33,5 +38,30 @@ class QuestionServiceTest {
 
         // THEN
         assertEquals(name, jihoon.getName());
+    }
+
+    @Test
+    @DisplayName("id에 해당하는 게시글을 찾습니다.")
+    void 게시글을_찾고_존재하지_않는다면_예외를_처리() {
+        // GIVEN
+        Long id1 = 1L; // 저장한 경우
+        Long id2 = 2L; // 저장하지 않은 경우
+
+        User user = new User();
+        User save = userRepo.save(user);
+        Question question1 = Question.builder()
+                .id(id1)
+                .user(save)
+                .build();
+        questionRepo.save(question1);
+
+        // WHEN
+        ResponseEntity<QuestionResponseDto> responseEntity1 = questionService.selectPost(id1);
+
+        // THEN
+        assertThrows(ResourceNotFoundException.class, () -> {
+            questionService.selectPost(id2);
+        });
+        assertEquals(question1.getTitle(), responseEntity1.getBody().getTitle());
     }
 }
