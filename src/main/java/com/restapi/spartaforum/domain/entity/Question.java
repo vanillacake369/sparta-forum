f129b945bee3877b7bfcfc2c3805fa9459aa3486
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-06 05:09:42 +0900

    feat : Question > 빠진 title,author,content 추가 이후 빌더패턴 적용

--- src/main/java/com/restapi/spartaforum/domain/entity/Question.java
+++ src/main/java/com/restapi/spartaforum/domain/entity/Question.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -19,9 +20,25 @@ public class Question extends TimeStamp {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String title;
+
+    private String author;
+
     private String password;
+
+    private String content;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @Builder
+    public Question(Long id, String title, String author, String password, String content, User user) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.password = password;
+        this.content = content;
+        this.user = user;
+    }
 }
