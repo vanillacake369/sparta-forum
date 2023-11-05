3c516fcc86a0f3de79c49fb902e1f33ee96395f7
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-06 05:08:23 +0900

    feat : dto 내부 필드 설정

--- src/main/java/com/restapi/spartaforum/domain/dto/QuestionRequestDto.java
+++ src/main/java/com/restapi/spartaforum/domain/dto/QuestionRequestDto.java
@@ -1,4 +1,18 @@
 package com.restapi.spartaforum.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class QuestionRequestDto {
+    private String title;
+
+    private String author;
+
+    private String password;
+
+    private String content;
 }
