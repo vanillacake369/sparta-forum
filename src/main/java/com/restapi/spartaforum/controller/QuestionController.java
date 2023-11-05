f6df315909c98e2d4175a3323316da5afd470ba3
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-06 06:25:22 +0900

    feat : QuestionController > 게시글 생성

--- src/main/java/com/restapi/spartaforum/controller/QuestionController.java
+++ src/main/java/com/restapi/spartaforum/controller/QuestionController.java
@@ -1,13 +1,23 @@
 package com.restapi.spartaforum.controller;
 
+import com.restapi.spartaforum.domain.dto.QuestionRequestDto;
+import com.restapi.spartaforum.domain.dto.QuestionResponseDto;
 import com.restapi.spartaforum.service.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/sparta-forum/questions")
 public class QuestionController {
-    private QuestionService questionService;
+    private final QuestionService questionService;
 
-    
+    @PostMapping("ask")
+    public ResponseEntity<QuestionResponseDto> createPost(@RequestBody QuestionRequestDto requestDto) {
+        return questionService.createPost(requestDto);
+    }
 }
